355f21d0c48a9369638c0bb980204130ad6eab1e
SAKSHAM TANEJA <samtaneja2002@gmail.com>
2026-05-08 12:07:45 +0530

    Add files via upload

---
+++ fizz_buzz.py
@@ -0,0 +1,22 @@
+def is_palindrome(s):
+    
+      result = ""
+      for char in s:
+            result = char + result
+      return result
+s=input("enter string:")
+c=is_palindrome(s)
+print(c)  
+    
+
+
+    
+
+
+
+    
+         
+     
+    
+    
+    
