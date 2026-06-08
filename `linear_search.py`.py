5fc7e38ec139edf0c47d98e740951a65a09556d1
SAKSHAM TANEJA <samtaneja2002@gmail.com>
2026-06-08 10:50:36 +0530

    Add files via upload

---
+++ `linear_search.py`.py
@@ -0,0 +1,32 @@
+arr=[10,20,30,40,50,60,70,80,90,100]
+val=50
+def check(arr,val):
+    for i in range(len(arr)):
+        if arr[i]==val:
+            print(f"{val} found at index {i}")
+            return True
+    return False
+a=check(arr,val)
+print(a)
+        
+
+
+
+
+
+        
+
+            
+
+
+                
+            
+
+
+
+
+            
+        
+            
+            
+    
